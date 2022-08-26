777911beaf96bad8288a0db240f147d4b887f06c
mbcodes <mbcodes@protonmail.com>
2022-08-25 22:04:28 -0700

    server updates

--- packages/server/server.js
+++ packages/server/server.js
@@ -1,13 +1,20 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import connectDB from './config/database.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+//connect DB
+connectDB();
+
+//connect passport
+
 //json + form POST
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`local host @ ${PORT}`);
