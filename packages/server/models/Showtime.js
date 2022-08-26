b7bf5a789af4e20588574ec93cf3f09d4658a596
mbcodes <mbcodes@protonmail.com>
2022-08-25 22:04:07 -0700

    initial models

---
+++ packages/server/models/Showtime.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+const ShowtimeSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  playwright: {
+    type: String,
+    required: true,
+  },
+  show_type: {
+    type: String,
+    required: false,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: false,
+  },
+  theatre_name: {
+    type: String,
+    required: true,
+  },
+  show_date: {
+    type: Date,
+    require: false,
+  },
+});
+
+const Showtime = mongoose.model('showtime', ShowtimeSchema);
+
+export default Showtime;
