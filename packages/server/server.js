d14e2c301c0523a1ef5ec4ca4e7318f6cb885c2b
mbcodes <mbcodes@protonmail.com>
2022-08-26 20:52:09 -0700

    added mongostore session + routes

--- packages/server/server.js
+++ packages/server/server.js
@@ -2,6 +2,9 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import connectDB from './config/database.js';
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
+import main from './routes/api/main.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,6 +18,24 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+
+//session
+app.use(
+  session({
+    secret: process.env.SESH_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+    }),
+  })
+);
+
+//routes
+app.get('/', (request, response) => {
+  response.send('API working');
+});
+app.use('/api/main', main);
 
 app.listen(PORT, () => {
   console.log(`local host @ ${PORT}`);
