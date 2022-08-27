fab4cbf7f034716bc141a8e950a43b9e0e4bb88d
mbcodes <mbcodes@protonmail.com>
2022-08-26 22:22:49 -0700

    server + dependencies addition

--- packages/server/server.js
+++ packages/server/server.js
@@ -12,11 +12,11 @@ const PORT = process.env.PORT || 5000;
 //connect DB
 connectDB();
 
-//connect passport
-
-//json + form POST
+//middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(cors());
 
 //session
