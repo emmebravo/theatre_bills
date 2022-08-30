e6240246fc8f460f47356a4b3556a4f0808ff295
mbcodes <mbcodes@protonmail.com>
2022-08-29 23:07:00 -0700

    added axios + react-router-dom

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -1,29 +1,29 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import ProtectedRoute from './components/PrivateRoute';
+import Feed from './components/Feed';
+import NotFound from './components/NotFound';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className='App'>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'></a>
-        <a href='https://reactjs.org' target='_blank'></a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Routes>
+      <Route exact path='/' element={<Home />} />
+      <Route exact path='/login' element={<Login />} />
+      <Route exact path='/register' element={<Register />} />
+      <Route
+        exact
+        path='/feed'
+        element={
+          <ProtectedRoute user={user}>
+            <Feed />
+          </ProtectedRoute>
+        }
+      />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
   );
 }
 
