fe5fbef7d93cb31b8e212d4c3625cae1d39232a0
mbcodes <mbcodes@protonmail.com>
2022-09-07 20:14:41 -0700

    styling updates

--- packages/client/tailwind.config.cjs
+++ packages/client/tailwind.config.cjs
@@ -8,13 +8,14 @@ module.exports = {
       lg: '976px',
       xl: '1440px',
     },
+
     extend: {
       colors: {
-        coral: 'rgba(255, 113, 36)',
-        green: 'rgba(142, 202, 193)',
-        purple: 'rgba(94, 42, 65)',
-        gray: 'rgba(250, 250, 250)',
-        darkGray: 'rgba(144, 149, 167)',
+        coral: 'rgb(255, 113, 36)',
+        green: 'rgb(142, 202, 193)',
+        purple: 'rgb(94, 42, 65)',
+        gray: 'rgb(250, 250, 250)',
+        darkGray: 'rgb(144, 149, 167)',
       },
     },
   },
