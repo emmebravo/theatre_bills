e816da3d841272f7840e542f190f8ee5dcabbef4
mbcodes <mbcodes@protonmail.com>
2022-09-20 22:23:08 -0700

    grammar fixes

--- packages/client/src/components/Hero.jsx
+++ packages/client/src/components/Hero.jsx
@@ -7,12 +7,12 @@ const Home = () => {
       {/* left */}
       <div className='flex flex-col mb-32 space-y-12 md:w-1/2'>
         <h1 className='max-w-md text-4xl font-bold text-center md:text-5xl md:text-left'>
-          <span className='text-coral'>Theatre Bills</span> where playbills go
+          <span className='text-coral'>Theatre Bills</span> where Playbills go
           digital
         </h1>
         <p className='max-w-sm text-center text-darkGray md:text-left'>
-          Welcome! Store your playbills digitally plus a ton more info: who you
-          went with, city and country of showing, theatre's name, and more!
+          Welcome! Store your playbills digitally plus a ton more info: date of
+          show, city of showing, theatre's name, and more!
         </p>
         <div className='flex space-x-2 justify-center md:justify-start'>
           <Link to='/register'>
