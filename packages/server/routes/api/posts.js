65bfbc311b2a986d77c2e6c75f81c390b1ef8ac6
mbcodes <mbcodes@protonmail.com>
2022-09-24 23:18:17 -0700

    commented out ensureAuth middleware

--- packages/server/routes/api/posts.js
+++ packages/server/routes/api/posts.js
@@ -1,22 +1,17 @@
 import express from 'express';
 import * as posts from '../../controllers/posts.js';
 import { upload } from '../../middleware/multer.js';
-import { ensureAuth } from '../../middleware/auth.js';
+//import { ensureAuth } from '../../middleware/auth.js';
 const router = express.Router();
 
 // @route POST api/posts/create-post
 // @desc create new show bill
 // @access Private
-router.post(
-  '/create-show',
-  ensureAuth,
-  upload.single('file'),
-  posts.createShow
-);
+router.post('/create-show', upload.single('image'), posts.createShow);
 
 // @route DELETE api/posts/delete-show/:id
 // @desc delete show entry
 // @access Private
-router.delete('/delete-show/:id', ensureAuth, posts.deleteShow);
+router.delete('/delete-show/:id', posts.deleteShow);
 
 export default router;
