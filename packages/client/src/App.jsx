6c38efb07bd3da11b426db072bfbd1a9d6ac1bb3
mbcodes <mbcodes@protonmail.com>
2022-09-30 21:31:11 -0700

    added protected route component

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -2,9 +2,10 @@ import { Route, Routes } from 'react-router-dom';
 import Hero from './components/Hero';
 import Login from './components/Login';
 import Register from './components/Register';
-import PrivateRoute from './components/PrivateRoute';
+import ProtectedRoute from './components/ProtectedRoute';
 import CreatePost from './components/CreatePost';
 import Feed from './components/Feed';
+// import Show from './components/Show';
 import Footer from './components/Footer';
 import NotFound from './components/NotFound';
 import Navigation from './components/Navigation';
@@ -13,22 +14,16 @@ function App() {
   return (
     <>
       <Navigation />
-      <div className='container mx-auto mt-10'>
+      <div className='container mx-auto my-10'>
         <Routes>
           <Route exact path='/' element={<Hero />} />
           <Route exact path='/login' element={<Login />} />
           <Route exact path='/register' element={<Register />} />
-          <Route exact path='/create-show' element={<CreatePost />} />
-          {/* <Route
-          exact
-          path='/feed'
-          element={
-            <ProtectedRoute user={user}>
-              <Feed />
-            </ProtectedRoute>
-          }
-        /> */}
-          <Route path='/feed' element={<Feed />} />
+          <Route element={<ProtectedRoute />}>
+            <Route exact path='/create-show' element={<CreatePost />} />
+            <Route exact path='/feed' element={<Feed />} />
+            {/* <Route exact path='/feed/:id' element={<Show />} /> */}
+          </Route>
           <Route path='*' element={<NotFound />} />
         </Routes>
       </div>
