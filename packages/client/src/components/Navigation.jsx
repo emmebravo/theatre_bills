4a1a25ad7038ae015ee1f5ab2f1bcc4e61782bd5
mbcodes <mbcodes@protonmail.com>
2022-09-30 21:31:38 -0700

    added hamburger menu, and specific css for it

--- packages/client/src/components/Navigation.jsx
+++ packages/client/src/components/Navigation.jsx
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Masks from '../assets/masks.svg';
 
 const Navigation = () => {
+  const [hamMenu, setHamMenu] = useState(false);
+
   return (
     <nav className='relative container mx-auto p-6'>
       <div className='flex items-center justify-between'>
@@ -11,8 +14,32 @@ const Navigation = () => {
           </Link>
         </div>
         <div className='hidden md:flex space-x-6'>
-          <Link to='/login'>Login</Link>
           <Link to='/register'>Register</Link>
+          <Link to='/login'>Login</Link>
+          <Link to='/'>About</Link>
+        </div>
+
+        {/* hamburger icon & menu */}
+        <button
+          onClick={() => setHamMenu((prev) => !prev)}
+          className={`block hamburger md:hidden focus:outline-none ${
+            hamMenu ? 'open' : ''
+          }`}
+        >
+          <span className='hamburger-top'></span>
+          <span className='hamburger-middle'></span>
+          <span className='hamburger-bottom'></span>
+        </button>
+      </div>
+
+      <div className='md:hidden'>
+        <div
+          className={`absolute flex-col items-center self-end py-8 mt-10 space-y-6 font-bold bg-gray sm:w-auto sm:self-center left-6 right-6 drop-shadow-md ${
+            hamMenu ? 'flex' : 'hidden'
+          }`}
+        >
+          <Link to='/register'>Register</Link>
+          <Link to='/login'>Login</Link>
           <Link to='/'>About</Link>
         </div>
       </div>
