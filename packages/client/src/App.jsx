62c583d806944f40b93bb24b31f0d536f8c1ac7c
mbcodes <mbcodes@protonmail.com>
2022-10-10 19:06:45 -0700

    cleaned up App component; removed Context

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -1,5 +1,5 @@
-import { useContext } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
 import Hero from './components/Hero';
 import Login from './components/Login';
 import Register from './components/Register';
@@ -7,32 +7,26 @@ import ProtectedRoute from './components/ProtectedRoute';
 import CreatePost from './components/CreatePost';
 import Feed from './components/Feed';
 // import Show from './components/Show';
-import Footer from './components/Footer';
 import NotFound from './components/NotFound';
-import Navigation from './components/Navigation';
-import { myContext } from './Context/Context';
 
 function App() {
-  const { user } = useContext(myContext);
-  console.log(user);
   return (
-    <>
-      <Navigation />
-      <div className='container mx-auto my-10'>
-        <Routes>
-          <Route exact path='/' element={<Hero />} />
-          <Route exact path='/login' element={<Login />} />
-          <Route exact path='/register' element={<Register />} />
-          <Route element={<ProtectedRoute user={user} />}>
-            <Route exact path='/create-show' element={<CreatePost />} />
-            <Route exact path='/feed' element={<Feed />} />
-            {/* <Route exact path='/feed/:id' element={<Show />} /> */}
-          </Route>
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-      </div>
-      <Footer />
-    </>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        {/* public routes */}
+        <Route exact path='/' element={<Hero />} />
+        <Route exact path='/login' element={<Login />} />
+        <Route exact path='/register' element={<Register />} />
+        <Route path='*' element={<NotFound />} />
+
+        {/* protected routes */}
+        <Route element={<ProtectedRoute />}>
+          <Route exact path='/feed' element={<Feed />} />
+          <Route exact path='/create-show' element={<CreatePost />} />
+          {/* <Route exact path='/feed/:id' element={<Show />} /> */}
+        </Route>
+      </Route>
+    </Routes>
   );
 }
 
