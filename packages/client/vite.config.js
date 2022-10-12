4f1d7bab4a98e52e42be426bedc80fd190896b25
mbcodes <mbcodes@protonmail.com>
2022-10-12 14:53:34 -0700

    config files

--- packages/client/vite.config.js
+++ packages/client/vite.config.js
@@ -4,10 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: './src/main.jsx',
-    },
-  },
 });
