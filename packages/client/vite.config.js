855e8bb8bddec3d882cbcd346e79cdda5d89ed23
mbcodes <mbcodes@protonmail.com>
2022-10-11 21:36:01 -0700

    changes to configs

--- packages/client/vite.config.js
+++ packages/client/vite.config.js
@@ -3,15 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-      },
+  plugins: [react()],
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: './src/main.jsx',
     },
   },
-  plugins: [react()],
 });
