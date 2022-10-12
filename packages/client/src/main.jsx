d90c05dc770df9139df13fc5c4776edc4da7b7c5
mbcodes <mbcodes@protonmail.com>
2022-10-12 15:27:15 -0700

    fixed module path

--- packages/client/src/main.jsx
+++ packages/client/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { AuthProvider } from '../src/context/AuthProvider';
+import { AuthProvider } from './context/AuthProvider';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
