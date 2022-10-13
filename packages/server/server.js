3e6e02d5973bd31d15b0cf30358a38d103cb9269
mbcodes <mbcodes@protonmail.com>
2022-10-12 18:52:36 -0700

    cors update

--- packages/server/server.js
+++ packages/server/server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: 'http://localhost:5173', // allows server to accept requests from this origin
+    origin: 'https://splendid-undershirt-slug.cyclic.app', // allows server to accept requests from this origin
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true, // allow cookie session from browser to pass through to server
   })
