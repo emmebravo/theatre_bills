f8438e46fb968617a7074f19dd5ac6247fb3a4e1
mbcodes <mbcodes@protonmail.com>
2023-05-21 17:10:40 -0700

    update users api routes

---
+++ packages/server/routes/api/auth.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import * as auth from '../../controllers/auth.js';
+const router = express.Router();
+
+// @route POST /users/register
+// @desc Register user
+// @access Public
+router.post('/register', auth.register);
+
+// @route POST /users/login
+// @desc Login user
+// @access Public
+router.post('/login', auth.login);
+
+// @route POST /users/logout
+// @desc Logout user
+// @access Private
+router.post('/logout', auth.logout);
+
+export default router;
--- packages/server/routes/api/main.js
+++ packages/server/routes/api/main.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import auth from './auth.js';
 import gets from './gets.js';
 import posts from './posts.js';
 
 const router = express.Router();
 
+router.use('/auth', auth);
 router.use('/gets', gets);
 router.use('/posts', posts);
 
