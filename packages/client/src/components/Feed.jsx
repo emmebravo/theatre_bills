089b83d4304ceb716be4f12308e96ff8130e4259
mbcodes <mbcodes@protonmail.com>
2023-05-21 18:19:14 -0700

    added if conditional

--- packages/client/src/components/Feed.jsx
+++ packages/client/src/components/Feed.jsx
@@ -21,32 +21,37 @@ const Feed = () => {
     fetchShow();
   }, []);
 
-  const shows = showData?.map((show) => (
-    <Card
-      key={show._id}
-      id={show._id}
-      image={show.image}
-      city={show.city}
-      title={show.title}
-      theatre={show.theatre_name}
-      playwright={show.playwright}
-      date={show.show_date}
-    />
-  ));
+  let shows;
+  if (!showData) {
+    shows = '';
+  } else {
+    shows = showData?.map((show) => (
+      <Card
+        key={show._id}
+        id={show._id}
+        image={show.image}
+        city={show.city}
+        title={show.title}
+        theatre={show.theatre_name}
+        playwright={show.playwright}
+        date={show.show_date}
+      />
+    ));
+  }
 
   return (
     <>
       <h2 className='max-w-md text-4xl font-bold text-center md:text-5xl md:text-left pb-4'>
         Shows
       </h2>
-      {showData?.length ? (
-        <div className='grid grid-cols-1 gap-4 grid-flow-row md:grid-cols-3'>
-          {shows}
-        </div>
-      ) : (
+      {!showData?.length ? (
         <h3 className='text-2xl font-bold'>
           You haven't digitized any playbills yet...
         </h3>
+      ) : (
+        <div className='grid grid-cols-1 gap-4 grid-flow-row md:grid-cols-3'>
+          {shows}
+        </div>
       )}
     </>
   );
